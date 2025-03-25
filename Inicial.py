2dc8ea9811cff072353c829851d9d55d42b273d4
lucas mesquita da silva <lucas.silva@grupocev.int>
2025-03-25 16:19:04 -0300

    final

--- Inicial.py
+++ Inicial.py
@@ -1,7 +1,7 @@
 import os 
 import streamlit as st
 import pandas as pd
-from auth import check_authentication
+from auth import check_authentication, logout
 
 st.set_page_config(page_title="Alteração de Dados", page_icon="🔄", layout="wide")
 
--- auth.py
+++ auth.py
@@ -1,34 +1,31 @@
 import streamlit as st
 
-def check_authentication(username, password):
-    try:
-        users = st.secrets["authentication"]  # Obtém a lista de usuários do secrets.toml
-        return username in users and users[username] == password
-    except KeyError:
-        st.error("Erro ao carregar credenciais. Verifique o arquivo secrets.toml.")
+def check_authentication():
+    if "authenticated" not in st.session_state:
+        st.session_state.authenticated = False
+    
+    if not st.session_state.authenticated:
+        st.warning("Faça login para acessar esta funcionalidade.")
+        login_form()
         return False
+    return True
 
 def login_form():
-    st.title("Login")
-    
+    users = st.secrets["authentication"]  # Obtém lista de usuários e senhas
+
     with st.form("login_form"):
         username = st.text_input("Usuário")
         password = st.text_input("Senha", type="password")
         submit_button = st.form_submit_button("Entrar")
 
         if submit_button:
-            if check_authentication(username, password):
+            if username in users and users[username] == password:
                 st.session_state.authenticated = True
                 st.success(f"Bem-vindo, {username}!")
                 st.rerun()
             else:
                 st.error("Usuário ou senha incorretos.")
 
-# Verifica se o usuário já está autenticado antes de exibir o login
-if "authenticated" not in st.session_state:
+def logout():
     st.session_state.authenticated = False
-
-if not st.session_state.authenticated:
-    login_form()
-else:
-    st.write("Você já está logado!")
+    st.rerun()
