6f6f59ef6dbd119ee1aedbbcc05a3766ce86f4b0
lucas mesquita da silva <lucas.silva@grupocev.int>
2025-03-28 13:27:57 -0300

    atualizar dados atuais

--- Inicial.py
+++ Inicial.py
@@ -106,6 +106,11 @@ st.subheader("📊 Dados Atuais dos Alunos")
 
 
 dados_atual = carregar_dados_alunos()
+if "RA" in dados_atual.columns:
+    dados_atual["RA"] = dados_atual["RA"].astype(str).str.zfill(7)
+else:
+    st.write("A coluna 'RA' não foi encontrada no arquivo.")
+
 
 if dados_atual.empty:
     try:
