1487aed5af30a1adc0cb8978e44830d47016d5e1
Lucas Mesquita da Silva <mesquita@MacBook-Air-de-Lucas.local>
2025-04-15 17:59:36 -0300

    Ajuste para Rec

--- pages/Rec.py
+++ pages/Rec.py
@@ -1,23 +1,53 @@
+import os
 import streamlit as st
 import pandas as pd
 import io
+
+ARQUIVOREC = 'arquivorec.xlsx'
 
 
 # Configuração da página
 st.set_page_config(page_title="Limpeza Dados REC", page_icon=" ", layout="wide")
 
-st.warning("Estamos em obra !!")
-st.stop()
+#st.warning("Estamos em obra !!")
+#st.stop()
     
 # Função para carregar o arquivo
-@st.cache_data
-def carregar_dados(arquivo):
-    try:
-        df = pd.read_excel(arquivo)
-        return df
-    except Exception as e:
-        st.error(f"Erro ao carregar o arquivo: {e}")
-        return pd.DataFrame()
+@st.cache_resource
+def carregar_dados_REC(opcao):
+    if os.path.exists(ARQUIVOREC):
+        if ARQUIVOREC.endswith('.xlsx'):
+            return pd.read_excel(ARQUIVOREC)
+        else:
+            st.warning("Formato de arquivo não suportado!")
+    else:
+            st.warning("Arquivo de dados dos alunos não encontrado!")
+    return pd.DataFrame()
+
+# Função para substituir o arquivo de alunos
+def substituir_arquivo_alunos(novo_arquivo, opcao):
+    file_extension = novo_arquivo.name.split('.')[-1].lower()
+    if file_extension == 'xlsx':
+        df = pd.read_excel(novo_arquivo)
+        #retirar o codigo em () do nome da disciplina
+            
+        df['VALOR'] = (
+            df['VALOR']
+            .str.replace(r'\s*\(.*?\)', '', regex=True)  # remove (qualquer coisa)
+            .str.replace(r'[\u200b\u200e\u202c\u00a0]', '', regex=True) 
+            .str.strip()
+        )
+            
+        df.rename(columns={'VALOR': 'DISCIPLINA',
+                            'NOME': 'ALUNO',
+                            'CODTURMA' : 'TURMADISC',
+                            'RA': 'RA'}, inplace=True)
+        df.to_excel(ARQUIVOREC, index=False)
+        df['RA'] = df['RA'].apply(lambda x: str(x).zfill(7))
+        st.success("Dados de alunos substituídos com sucesso!")
+    else:
+        st.warning("Formato de arquivo não suportado para substituição!")
+
 
 # Função para limpar os dados
 @st.cache_data
@@ -31,9 +61,11 @@ def limpar_dados(df, prova, etapa, codetapa, codprova, tipoetapa):
                             'NOMECURSO': 'CURSO',
                             'NOMEALUNO': 'ALUNO'}, inplace=True)
     
-    df.rename(columns={'NOMEDISCIPLINA': 'DISCIPLINA',
-                       'NOMECURSO': 'CURSO',
-                       'NOMEALUNO': 'ALUNO'}, inplace=True)
+    df['nomelimpo'] = df['VALOR'].str.lower().str.strip()
+    df.rename(columns={'nomelimpo': 'DISCIPLINA',
+                       'CODTURMA': 'TURMADISC',
+                       'NOME': 'ALUNO',
+                       'RA': 'RA'}, inplace=True)
     
     df = pd.merge(df_base, df[['DISCIPLINA', 'ALUNO', 'RA',  'NOTAS']],
                   on=['DISCIPLINA', 'ALUNO', 'RA'],
@@ -55,57 +87,85 @@ def limpar_dados(df, prova, etapa, codetapa, codprova, tipoetapa):
 
     return df_teste
 
+def dash(df):
+    if not df:
+        st.write("Data frame Vazio")
+        return pd.DataFrame() 
+    if not os.path.exists(df):
+        st.write(f"Erro: Arquivo '{df}' não encontrado.")
+        return pd.DataFrame()  
+    return pd.read_excel(df) 
+
 # Interface do Streamlit
 st.title("Limpeza e tratamento de notas de REC")
 
 # Upload do arquivo Excel
-uploaded_file = st.file_uploader("📤 Envie o arquivo de notas (Excel)", type=["xlsx"])
+uploaded_file = st.file_uploader("Escolha um arquivo Excel da Rec para analise", type=["xlsx"])
 
-# Definir as variáveis de configuração para o filtro
-etapa = st.selectbox('Selecione a etapa', ['REC_P1', 'REC_P2'])
-tipoetapa = 'N'  # Tipo de etapa
-codetapa = 2  # Código da etapa
-codprova = 1  # Código da prova
-prova = 'Recuperação'
+if uploaded_file is not None:
+    df_novo = pd.read_excel(uploaded_file)
+    
+    st.write("📋 Prévia do arquivo enviado:")
+    st.write(f"Total de linhas: {len(df_novo)}")
+    st.write(f"Colunas: {', '.join(df_novo.columns)}")
+    st.dataframe(df_novo.head())
 
-if etapa == 'REC_P1' :
-    codetapa = 1  # P1 = 1
-    codprova = 2  # Recuperação = 2
-elif etapa == 'REC_P2':
-    codetapa = 2  # P2 = 2
-    codprova = 2  # Recuperação = 2
+    if st.button("🔄 Substituir Dados"):
+        substituir_arquivo_alunos(uploaded_file, ARQUIVOREC)
 
-# Carregar e limpar os dados
-if uploaded_file:
-    df_original = carregar_dados(uploaded_file)
-    st.subheader("📋 Dados Originais")
-    st.dataframe(df_original)
-    
-    disciplina = df_original['DISCIPLINA'].iloc[0]
-    turma = df_original['TURMADISC'].iloc[0]
-    
-    # Limpar dados
-    df_limpo = limpar_dados(df_original, prova, etapa, codetapa, codprova, tipoetapa)
-    st.subheader("✅ Dados Após Limpeza")
-    st.dataframe(df_limpo)
-    
-    
-    df_limpo['RA'] = df_limpo['RA'].astype(str)
-    df_limpo['RA'] = df_limpo['RA'].apply(lambda x: str(x).zfill(7))
-    df_limpo['NOTAS'] = df_limpo['NOTAS'].apply(lambda x: f"{x:.2f}".replace('.', ','))
-    
-    # Criar o arquivo .txt com separador ';'
-    output = io.BytesIO()  
-    df_limpo.to_csv(output, index=False, sep=';', encoding='utf-8', header=False)
-    output.seek(0) 
+        
+st.subheader("Dados dos Cadastrados na REC")
+if not ARQUIVOREC:
+    st.write("Data frame Vazio")
+elif not os.path.exists(ARQUIVOREC):  
+    st.write(f"O arquivo '{ARQUIVOREC}' não existe. Verifique o caminho ou envie o arquivo. ")
+else:
+    dados_disciplina = dash(ARQUIVOREC)
+    if not dados_disciplina.empty:  # Verifica se o DataFrame não está vazio
+        st.dataframe(dados_disciplina[['DISCIPLINA','ALUNO']]) # teste 
 
+
+def gerar_excel(df_rec, disciplina, turma):
+    df_filtrado = df_rec[(df_rec["DISCIPLINA"] == disciplina) & (df_rec["TURMADISC"] == turma)]
+    df_filtrado['RA'] = df_filtrado['RA'].astype(str)
+    df_filtrado["NOTAS"] = 0
+    colunas = ['TURMADISC', 'DISCIPLINA', 'RA', 'ALUNO', 'NOTAS']
+    df_filtrado = df_filtrado[colunas]
     
-    # Botão para baixar o arquivo tratado como .txt
+    output = io.BytesIO()
+    with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+        df_filtrado.to_excel(writer, index=False, sheet_name="Notas")
+    output.seek(0)
+    return output
+
+st.title("Gerador de Planilha de Notas para REC")
+
+df_rec = dash(ARQUIVOREC)
+if df_rec.empty:
+    st.stop()
+    
+disciplinas = df_rec["DISCIPLINA"].unique().tolist()
+disciplina = st.selectbox("📖 Escolha a disciplina", disciplinas)
+
+turmas_filtradas = df_rec[df_rec["DISCIPLINA"] == disciplina]["TURMADISC"].unique().tolist()
+turma = st.selectbox("🏫 Escolha a turma", turmas_filtradas)
+
+prova = st.selectbox("Escolha se é REC_P1 ou REC_P2", ["REC_P1", "REC_P2"])
+
+df_filtrado = df_rec[(df_rec["DISCIPLINA"] == disciplina) & (df_rec["TURMADISC"] == turma)]
+st.write(f"📝 **Alunos da Disciplina: {disciplina} | Turma: {turma}**")
+total = df_filtrado['ALUNO'].count()
+st.write(f"📝 **Quatidade de REC solicitadas**: {total}")
+st.dataframe(df_filtrado[["ALUNO", "DISCIPLINA", "TURMADISC"]])
+
+
+if disciplina and turma:
+    excel_file = gerar_excel(df_rec, disciplina, turma)
     st.download_button(
-        label="⬇ Baixar Notas Tratadas (TXT)",
-        data=output,
-        file_name=f"{disciplina}_{turma}_{prova}.txt",
-        mime="text/plain"
+        label="📥 Gerar e Baixar Planilha Excel",
+        data=excel_file,
+        file_name=f"{disciplina}_{turma}_{prova}.xlsx",
+        mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
     )
-
+        
     
