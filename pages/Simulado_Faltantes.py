aa9670867278b3780ede03345e6abcab8f9046c1
Mesquita02 <lmesquitas@gmail.com>
2025-06-25 10:55:37 -0300

    Simulado Faltantes ADM

--- pages/Simulado_Faltantes.py
+++ pages/Simulado_Faltantes.py
@@ -13,37 +13,37 @@ df_alunos = st.session_state["dados"].get("alunosxdisciplinas").copy()
 df_base = df_alunos.copy()
 
 
-padrao_remover = r'(Projeto de Extensão|Seminários|Liga dos Campeões|TCC|Estágio|Trabalho de Conclusão de Curso)'
+padrao_remover = r'(?:Projeto de Extensão|Seminários|Liga dos Campeões|TCC|Estágio|Trabalho de Conclusão de Curso)'
 df_base = df_base[
     ~df_base['DISCIPLINA'].str.contains(padrao_remover, case=False, na=False) &
     df_base['TURMADISC'].astype(str).str.len().le(4)
 ].reset_index(drop=True)
 
 # Renomear colunas para o padrão usado nas demais páginas
-df_alunos.rename(columns={
+df_base.rename(columns={
     'NOMEDISCIPLINA': 'DISCIPLINA',
     'NOMECURSO': 'CURSO',
     'NOMEALUNO': 'ALUNO'
 }, inplace=True)
 
 # Garantir RA com 7 dígitos
-df_alunos['RA'] = df_alunos['RA'].astype(str).str.zfill(7)
+df_base['RA'] = df_base['RA'].astype(str).str.zfill(7)
 
 st.title("Gerar Notas Zero para Alunos Faltantes")
 
 # Filtros: Curso e Turma
-cursos = sorted(df_alunos['CURSO'].dropna().unique())
+cursos = sorted(df_base['CURSO'].dropna().unique())
 curso_selecionado = st.selectbox("Selecione o curso:", cursos, index=None)
 
 if curso_selecionado:
-    turmas = sorted(df_alunos[df_alunos['CURSO'] == curso_selecionado]['TURMADISC'].dropna().unique())
+    turmas = sorted(df_base[df_base['CURSO'] == curso_selecionado]['TURMADISC'].dropna().unique())
     turmas_selecionadas = st.multiselect("Selecione a(s) Turma(s):", turmas)
 
     if turmas_selecionadas:
         # Filtrar alunos dessas turmas
-        df_filtrado = df_alunos[
-            (df_alunos['CURSO'] == curso_selecionado) &
-            (df_alunos['TURMADISC'].isin(turmas_selecionadas))
+        df_filtrado = df_base[
+            (df_base['CURSO'] == curso_selecionado) &
+            (df_base['TURMADISC'].isin(turmas_selecionadas))
         ]
 
         # Multi-select de alunos faltantes
