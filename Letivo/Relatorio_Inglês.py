6bcae779e9f4dac7a49515b1a71852a60fee61b8
Mesquita02 <lmesquitas@gmail.com>
2025-08-18 14:54:06 -0300

    nivelamento

--- Letivo/Relatorio_Inglês.py
+++ Letivo/Relatorio_Inglês.py
@@ -29,14 +29,14 @@ def carregar():
         header_paragraph = header.paragraphs[0]  # pega o parágrafo existente
         header_paragraph.alignment = WD_ALIGN_PARAGRAPH.CENTER  # centraliza o parágrafo
         run = header_paragraph.add_run()
-        run.add_picture(imagem_cabecalho, width=Inches(8))  # largura da imagem
+        run.add_picture(imagem_cabecalho, width=Inches(10))  # largura da imagem
 
         # Rodapé centralizado
         footer = section.footer
         footer_paragraph = footer.paragraphs[0]
         footer_paragraph.alignment = WD_ALIGN_PARAGRAPH.CENTER
         run_footer = footer_paragraph.add_run()
-        run_footer.add_picture(imagem_rodape, width=Inches(8))
+        run_footer.add_picture(imagem_rodape, width=Inches(10)))
 
         # Conteúdo
         doc.add_paragraph(f"Curso: {curso}", style='Heading 2')
