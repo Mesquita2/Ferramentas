cab031394c2bfaf0b67ceca9539fca6446abaa2e
Mesquita02 <lmesquitas@gmail.com>
2025-08-26 17:25:17 -0300

    usou local do tcc

--- Letivo/TCC.py
+++ Letivo/TCC.py
@@ -20,20 +20,12 @@ def carregar():
     def iniciar_driver():
         options = Options()
         options.add_argument("--headless=new")
+        options.add_argument("--disable-gpu")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
-        options.add_argument("--disable-gpu")
         options.add_argument("--window-size=1920,1080")
-
-        # Força usar o chromium do container do Streamlit Cloud
-        options.binary_location = "/usr/bin/chromium-browser"
-
-        driver = webdriver.Chrome(
-            executable_path="/usr/bin/chromedriver",
-            options=options
-        )
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
         return driver
-
 
     def login_moodle(usuario, senha):
         driver = iniciar_driver()
@@ -183,10 +175,4 @@ def carregar():
 
             else:
                 st.warning("Nenhum envio encontrado ou sem permissão.")
-                
-            if st.button("Encerrar sessão"):
-                if st.session_state.driver:
-                    st.session_state.driver.quit()
-                    st.session_state.driver = None
-
             
