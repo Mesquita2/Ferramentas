fb68ebd424cdc1d1138b99cb63d511ee22830b45
Mesquita02 <lmesquitas@gmail.com>
2025-08-26 16:41:40 -0300

    Projeto TCC com selenium

--- Letivo/TCC.py
+++ Letivo/TCC.py
@@ -19,7 +19,7 @@ def carregar():
 
     def iniciar_driver():
         options = Options()
-        options.add_argument("--headless")
+        options.add_argument("--headless=new")
         options.add_argument("--disable-gpu")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
