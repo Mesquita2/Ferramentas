b78ed1923a55539e31ea4c285d114ab47ef9e9ff
Mesquita02 <lmesquitas@gmail.com>
2025-10-24 17:14:46 -0300

    Ajuste rec

--- Letivo/ConversorTotvs.py
+++ Letivo/ConversorTotvs.py
@@ -145,7 +145,7 @@ def carregar():
                 etapa = match.group(2).upper()
 
                 # Normaliza "REC" → "RECUPERAÇÃO"
-                if tipo_raw in ["Rec"]:
+                if tipo_raw in ["REC", "RECUPERAÇÃO"]:
                     tipo = "RECUPERAÇÃO"
                 if tipo_raw in ["QUIZ", "QUIZZ"]:
                     tipo = "QUIZZ"
