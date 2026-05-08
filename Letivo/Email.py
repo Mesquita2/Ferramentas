5bdf727823809365de1fbfda740813927da9ceb5
Mesquita02 <lmesquitas@gmail.com>
2026-05-08 17:32:26 -0300

    ajuste coluna de nome

--- Letivo/Email.py
+++ Letivo/Email.py
@@ -161,7 +161,7 @@ def carregar():
             if turmas_selecionadas:
                 quantidade_total = (
                     df_base[(df_base["DISCIPLINA"] == disciplina) & 
-                            (df_base["TURMADISC"].isin(turmas_selecionadas))]['ALUNO'].count() + (5 * len(turmas_selecionadas))
+                            (df_base["TURMADISC"].isin(turmas_selecionadas))]['ALU_NOME'].count() + (5 * len(turmas_selecionadas))
                 )
 
                 st.markdown(f"**Quantidade de cópias (total):** {quantidade_total}")
@@ -213,7 +213,7 @@ def carregar():
                         for turma in turmas_selecionadas:
                             assunto, mensagem = create_assunto(
                                 curso, disciplina,
-                                df_base[(df_base["DISCIPLINA"] == disciplina) & (df_base["TURMADISC"] == turma)]['ALUNO'].count() + 5,
+                                df_base[(df_base["DISCIPLINA"] == disciplina) & (df_base["TURMADISC"] == turma)]['ALU_NOME'].count() + 5,
                                 tipo, tipo_prova, data_aplicar, turma
                             )
 
